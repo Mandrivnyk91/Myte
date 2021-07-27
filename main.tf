4a79cd0a7dc5ed9ba545f64d6dfc569d9150f55b
Volodymyr Katynskyi <v.katynskyi@ualwo9610nbw.levi9.com>
2021-07-27 23:42:09 +0300

    change tf backend

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
 terraform {
-      backend "remote" {
-          
-        organization = "Myte-mandr"
-
-        workspaces {
-          name = "Myte-test"
-        }
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "Myte-mandr"
+
+    workspaces {
+      name = "Myte-test"
+    }
+  }
+}    
 
 provider "aws" {
   region = "eu-west-1"
